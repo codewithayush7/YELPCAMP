8bb9d5ffc9183d3ee14034b9fe9f0fc780cfc078
codewithayush7 <codewithayush7@gmail.com>
2025-04-16 23:18:20 +0530

    final2

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const userRoutes=require('./routes/user');
 const campgroundRoutes=require('./routes/campground');
 const reviewRoutes=require('./routes/reviews');
 
-const dbUrl='mongodb://localhost:27017/yelp-camp';
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("Connected to MongoDB!");
